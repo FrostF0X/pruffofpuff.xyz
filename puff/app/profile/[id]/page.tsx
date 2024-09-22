4bebe6fd708d054f0d1ab5244bec01f3372298ea
dims <dmytro.konowalow@gmail.com>
2024-09-22 10:46:26 +0800

    build

--- puff/app/profile/[id]/page.tsx
+++ puff/app/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const runtime = 'edge';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useParams, useRouter} from 'next/navigation';
--- puff/app/profile/redeem/[id]/page.tsx
+++ puff/app/profile/redeem/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const runtime = 'edge';
 
 import React, {Suspense, useEffect, useState} from 'react';
 import {useParams, useSearchParams, useRouter} from 'next/navigation';
