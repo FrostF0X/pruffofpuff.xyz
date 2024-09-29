e92bbd76317450f404fd53c46704aa4d1dae6dc1
dims <dmytro.konowalow@gmail.com>
2024-09-29 23:30:29 +0800

    fix magic link auth

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://vercel.live https://*.vercel.app https://www.google.com https://www.gstatic.com https://*.google.com https://www.paypal.com https://www.paypalobjects.com https://auth.magic.link;
+    frame-src 'self' https://secure.walletconnect.org/;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://vercel.live https://*.vercel.app https://www.google.com https://www.gstatic.com https://*.google.com https://www.paypal.com https://www.paypalobjects.com https://auth.magic.link https://pruffofpuff-xyz.vercel.app;
     style-src 'self' 'unsafe-inline';
+    connect-src https://api.web3modal.org;
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
