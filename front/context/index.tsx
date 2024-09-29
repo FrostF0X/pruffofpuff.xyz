3658ae659c4f634b0210941bb36f3d75a3245bb0
dims <dmytro.konowalow@gmail.com>
2024-09-29 23:25:07 +0800

    fix magic link auth

--- front/context/index.tsx
+++ front/context/index.tsx
@@ -16,8 +16,8 @@ createWeb3Modal({
     metadata,
     wagmiConfig: config,
     projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
-    enableOnramp: true, // Optional - false as default
+    enableAnalytics: false, // Optional - defaults to your Cloud configuration
+    enableOnramp: false, // Optional - false as default
     themeMode: "light", // By default - set to user system settings
     themeVariables: {
         "--w3m-font-family": "Verdana", // Base font family
--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://vercel.live https://*.vercel.app https://www.google.com https://www.gstatic.com https://*.google.com https://www.paypal.com https://www.paypalobjects.com https://auth.magic.link;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ]
+    },
+};
 
 export default nextConfig;
